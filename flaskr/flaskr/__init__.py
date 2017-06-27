c5d39cd6d37d8eb322202d419b735ffdf3a25331
root <root@query17-14.ing.puc.cl>
2017-06-27 17:33:44 -0400

    Collection

--- flaskr/flaskr/__init__.py
+++ flaskr/flaskr/__init__.py
@@ -17,7 +17,7 @@ def create_app():
 app = create_app()
 
 # REPLACE WITH YOUR DATABASE NAME
-MONGODATABASE = "myDatabase"
+MONGODATABASE = "test"
 MONGOSERVER = "localhost"
 MONGOPORT = 27017
 client = MongoClient(MONGOSERVER, MONGOPORT)
@@ -35,7 +35,7 @@ postgresdb = psycopg2.connect(
 '''
 
 #Cambiar por Path Absoluto en el servidor
-QUERIES_FILENAME = 'queries'
+QUERIES_FILENAME = '/var/www/flaskr/flaskr/queries.json'
 
 
 @app.route("/")
